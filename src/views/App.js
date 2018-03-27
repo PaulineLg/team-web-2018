146db2f19c0caab964112a9df9b476fa7f3e299a
Damagae <daphnegm.rose@gmail.com>
2018-03-27 19:13:38 +0200

    add folders assets and views in /src

--- src/views/App.js
+++ src/views/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './crepe.png';
-import './App.css';
+import logo from '../crepe.png';
+import '../assets/css/App.css';
 
 class App extends Component {
   render() {
