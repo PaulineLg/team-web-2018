283192ba448840f3173baef118f32f1f35da5bd5
Unknown <paulinedivry@aol.com>
2018-04-30 12:31:27 +0200

    Begin user view

---
+++ src/views/User.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import '../assets/css/App.css';
+import Card from '../components/Card.jsx';
+import Sidebar from '../components/Sidebar.jsx';
+import ShippingGraph from '../components/ShippingGraph.jsx';
+import WelcomePage from '../components/WelcomePage.jsx';
+import logo from '../assets/img/logo-flower.png';
+import axios from 'axios'
+import UpkeepPlants from '../components/UpkeepPlants.jsx';
+
+class User extends Component {
+  constructor() {
+   super()
+
+ }
+
+  render() {
+    if (this.userConnected()) {
+      return (this.connectedInterface())
+    }
+    else {
+      return (this.disconnectedInterface())
+    }
+
+  }
+}
+
+export default User;
