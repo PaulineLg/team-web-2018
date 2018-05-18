4616ff4e2a374232ac885206ba49518886123019
Unknown <paulinedivry@aol.com>
2018-05-18 12:10:02 +0200

    Continue Plant List with state

--- src/views/Plants.js
+++ src/views/Plants.js
@@ -12,26 +12,56 @@ import TreatmentPlants from '../components/TreatmentPlants.jsx';
 import MapContainer from '../components/MapContainer.js';
 
 class Plants extends Component {
-  state = {
-    plants: ['tulip', 'rose'],
-    currentPlant: 'tulip'
-  }
+    constructor(props) {
+        super(props)
+        this.handleNextPlant = this.handleNextPlant.bind(this);
+        this.handlePrevPlant = this.handlePrevPlant.bind(this);
+        this.state = {
+            plants: ['Tulip', 'Rose', 'Muguet', 'Orchidées'],
+            currentPlant: 0
+        }
+      }
 
- // handleNextPlant()
+handleNextPlant = (newCurrent) => {
+    console.log(this.state.currentPlant);
+    this.setState((preState) => {
+        if(this.state.currentPlant >= this.state.plants.length){
+           return  {
+               currentPlant : preState.currentPlant-this.state.plants.length
+           }
+        }
+        return {
+            currentPlant : preState.currentPlant++
+        };
+    });
+}
+
+ handlePrevPlant = (newCurrent) => {
+    console.log(this.state.currentPlant);
+    this.setState((preState) => {
+        if(this.state.currentPlant <= -1){
+           return  {
+               currentPlant : this.state.plants.length-1
+           }
+        }
+        return {
+            currentPlant : preState.currentPlant--
+        };
+    });
+ }
 
   PlantsView() {
     return (
-
         <div className="Plants">
             <h1>MY PLANTS</h1>
+            Hello {this.props.user.name}, your watering level for this plants
             <div id="plantsList">
-                Hello {this.props.user.name}, your watering level for this plants
-                <div>&lt;</div>
-                <div>{this.state.currentPlant}</div>
-                <div>&gt;</div>
+                <div id="button-prev" onClick={() => this.handlePrevPlant(this.state.currentPlant--)}><span>&lt;</span></div>
+                <div id="listState">{this.state.plants[this.state.currentPlant]}</div>
+                <div id="button-next" onClick={() => this.handleNextPlant(this.state.currentPlant++)}><div>&gt;</div></div>
             </div>
             <div id="water">
-                    <WaterPlants />
+                <WaterPlants />
             </div>
             <div id="treatment">
                 <TreatmentPlants />
