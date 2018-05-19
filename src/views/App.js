93ee448f921880844158ed8d6d4b30e7bfd8f568
Damagae <daphnegm.rose@gmail.com>
2018-05-19 16:38:52 +0200

    Orders and home display

--- src/views/App.js
+++ src/views/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import '../assets/css/App.css';
 import Card from '../components/Card.jsx';
 import Sidebar from '../components/Sidebar.jsx';
-import ShippingGraph from '../components/ShippingGraph.jsx';
 import WelcomePage from '../components/WelcomePage.jsx';
 import logo from '../assets/img/logo-flower.png';
 import axios from 'axios'
--- src/views/User.js
+++ src/views/User.js
@@ -3,34 +3,47 @@ import '../assets/css/App.css';
 import Card from '../components/Card.jsx';
 import Sidebar from '../components/Sidebar.jsx';
 import ShippingGraph from '../components/ShippingGraph.jsx';
-import logo from '../assets/img/logo-flower.png';
+import SalesGraph from '../components/SalesGraph.jsx';
 import axios from 'axios'
 import UpkeepPlants from '../components/UpkeepPlants.jsx';
 import { GoogleApiWrapper } from 'google-maps-react'
 import MapContainer from '../components/MapContainer'
 
 class User extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: this.props.user
+    }
+  }
+  loadSales() {
+
+  }
   UserView() {
     return (
       <div className="User">
         <h1>USER STATISTICS</h1>
 
-        <div id="OrdersMap">
-            <MapContainer google={this.props.google} user={this.props.user} />
-            <div id="orders-list">Hello {this.props.user.name}, <br/>
+        <div className="OrdersMap">
+            <MapContainer google={this.props.google} user={this.state.user} />
+            <div id="orders-list">Hello {this.state.user.name}, <br/>
             The adresses of the next 3 deliveries to make : <br/>
             New York County Supreme Court <br/>
             Queens County Supreme Court <br/>
             Kings County Supreme Court <br/>
             </div>
         </div>
-        <div id="ShippingGraph" >
-          <ShippingGraph />
+        <div className='chart-container'>
+          <div id="ShippingGraph" className="chart" >
+            <ShippingGraph />
+          </div>
+          <div id="SalesGraph" className="chart" >
+            <SalesGraph />
+          </div>
+          <div id="UpkeepPlants" className="chart" >
+            <UpkeepPlants />
+          </div>
         </div>
-        <div id="UpkeepPlants">
-          <UpkeepPlants />
-        </div>
-        <div id='chart-container'></div>
       </div>
     )
   }
